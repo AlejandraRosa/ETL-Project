2370c8a152acdb487ecc78c34031bec73bb5f8b2
Alejandra Rosa <57076874+AlejandraRosa@users.noreply.github.com>
2020-02-22 12:37:31 -0600

    final touches

--- ETL.sql
+++ ETL.sql
@@ -6,14 +6,14 @@ drop table economic_freedom;
 --Create Tables--
 
 create table economic_freedom(
-	iso_code VARCHAR,
-	country_name VARCHAR,
+	iso_code VARCHAR(3),
+	country_name VARCHAR(40),
 	economic_freedom_summary_index DEC(3,2),
 	economic_freedom_rank INT);
 
 create table life_expectancy (
-	iso_code VARCHAR,
-	country_name VARCHAR,
+	iso_code VARCHAR(3),
+	country_name VARCHAR(40),
 	life_expectancy DEC(5,2));
 
 
@@ -38,4 +38,3 @@ inner join life_expectancy on
 economic_freedom.iso_code = life_expectancy.iso_code;
 
 select * from combined order by economic_freedom_rank asc;
-
